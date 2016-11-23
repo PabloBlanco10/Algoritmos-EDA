b3649505852bd0b7613ca4bd0855be6b5408140a
PabloBlanco10 <pablo_bp94@hotmail.com>
2016-11-23 02:52:53 +0100

    corregido

--- p7.cpp
+++ p7.cpp
@@ -10,119 +10,119 @@
 #include <iostream>
 using namespace std;
 
-
-bool evaluaExpresion(int a, int b, int c, int d, bool &ret, int &p1, int &p2){
-    bool r;
-    int e, f, g, h;
-    if((a != 0) && (b != 0) && (c != 0) && (d != 0)){
-        if ((a*b) != (c*d)){
-            ret = false;
-            return false;
-        }
-        else {
-            p1 = a;
-            p2 = c;
-        }
-       // r = ((a*b) == (c*d));
-//        cout << "ninguno es cero" << endl;
-    }
-    else{
-    
-        if(a == 0){
-            cin >> e;
-            cin >> f;
-            cin >> g;
-            cin >> h;
-            p1 = e + g;
-            evaluaExpresion(e,f,g,h, ret, p1, p2);
-           // peso1 += e + g;
-           // cout << "peso1 = " << peso1 << endl;
-        }
-        if(b == 0){
-            cin >> e;
-            cin >> f;
-            cin >> g;
-            cin >> h;
-            evaluaExpresion(e,f,g,h, ret, p1, p2);
-        }
-        if(c == 0){
-            cin >> e;
-            cin >> f;
-            cin >> g;
-            cin >> h;
-            p2 = e + g;
-            evaluaExpresion(e,f,g,h, ret, p1, p2);
-            //peso1 += e + g;
-           // cout << "peso2 = " << peso2 << endl;
-        }
-        //else peso2 += c;
-        
-        if(d == 0){
-            cin >> e;
-            cin >> f;
-            cin >> g;
-            cin >> h;
-            evaluaExpresion(e,f,g,h, ret, p1, p2);
-        }
-    }
-    
-    if ((a*b) != (c*d))
-        ret = false;
-    if ((e*f) != (g*h))
-        ret = false;
-    p1 = p1 * b;
-    p2 = p2 * d;
-//    if((p1) != (p2))
+//
+//bool evaluaExpresion(int a, int b, int c, int d, bool &ret, int &p1, int &p2){
+//    bool r;
+//    int e, f, g, h;
+//    if((a != 0) && (b != 0) && (c != 0) && (d != 0)){
+//        if ((a*b) != (c*d)){
+//            ret = false;
+//            return false;
+//        }
+//        else {
+//            p1 = a;
+//            p2 = c;
+//        }
+//       // r = ((a*b) == (c*d));
+////        cout << "ninguno es cero" << endl;
+//    }
+//    else{
+//    
+//        if(a == 0){
+//            cin >> e;
+//            cin >> f;
+//            cin >> g;
+//            cin >> h;
+//            p1 = e + g;
+//            evaluaExpresion(e,f,g,h, ret, p1, p2);
+//           // peso1 += e + g;
+//           // cout << "peso1 = " << peso1 << endl;
+//        }
+//        if(b == 0){
+//            cin >> e;
+//            cin >> f;
+//            cin >> g;
+//            cin >> h;
+//            evaluaExpresion(e,f,g,h, ret, p1, p2);
+//        }
+//        if(c == 0){
+//            cin >> e;
+//            cin >> f;
+//            cin >> g;
+//            cin >> h;
+//            p2 = e + g;
+//            evaluaExpresion(e,f,g,h, ret, p1, p2);
+//            //peso1 += e + g;
+//           // cout << "peso2 = " << peso2 << endl;
+//        }
+//        //else peso2 += c;
+//        
+//        if(d == 0){
+//            cin >> e;
+//            cin >> f;
+//            cin >> g;
+//            cin >> h;
+//            evaluaExpresion(e,f,g,h, ret, p1, p2);
+//        }
+//    }
+//    
+//    if ((a*b) != (c*d))
 //        ret = false;
-    
-    
-    cout << "peso 1 final: "  << p1 << endl;
-    cout << "peso 2 final: "  << p2 << endl;
-
-    r = ret;
-    return r;
-}
-
-void resuelve(int a, int b, int c, int d){
-    bool si = false;
-    bool p = true;
-    int p1 = 0;
-    if (evaluaExpresion(a,b,c,d,p,p1,p1))
-        cout << "SI" << endl;
-    
-    else
-        cout << "NO" << endl;
-//    cout << evaluaExpresion(a, b, c, d) << endl;
-//    cout << "resuelve hecho" << endl;
-}
-
-int main(){
-    int a, b, c, d;
-    cin >> a;
-    cin >> b;
-    cin >> c;
-    cin >> d;
-    bool ceros = true;
-
-    while(ceros){
-        
-        resuelve(a,b,c,d);
-        cin >> a;
-        cin >> b;
-        cin >> c;
-        cin >> d;
-        if ((a == 0) && (b == 0) && (c == 0) && (d == 0)) {
-            ceros = false;
-        }
-//        cout << " si entra al while" << endl;
-    }
-    
-    return 0;
-    
-}
-
-
-
+//    if ((e*f) != (g*h))
+//        ret = false;
+//    p1 = p1 * b;
+//    p2 = p2 * d;
+////    if((p1) != (p2))
+////        ret = false;
+//    
+//    
+//    cout << "peso 1 final: "  << p1 << endl;
+//    cout << "peso 2 final: "  << p2 << endl;
+//
+//    r = ret;
+//    return r;
+//}
+//
+//void resuelve(int a, int b, int c, int d){
+//    bool si = false;
+//    bool p = true;
+//    int p1 = 0;
+//    if (evaluaExpresion(a,b,c,d,p,p1,p1))
+//        cout << "SI" << endl;
+//    
+//    else
+//        cout << "NO" << endl;
+////    cout << evaluaExpresion(a, b, c, d) << endl;
+////    cout << "resuelve hecho" << endl;
+//}
+//
+//int main(){
+//    int a, b, c, d;
+//    cin >> a;
+//    cin >> b;
+//    cin >> c;
+//    cin >> d;
+//    bool ceros = true;
+//
+//    while(ceros){
+//        
+//        resuelve(a,b,c,d);
+//        cin >> a;
+//        cin >> b;
+//        cin >> c;
+//        cin >> d;
+//        if ((a == 0) && (b == 0) && (c == 0) && (d == 0)) {
+//            ceros = false;
+//        }
+////        cout << " si entra al while" << endl;
+//    }
+//    
+//    return 0;
+//    
+//}
+//
+//
+//
 
 
 
@@ -149,12 +149,35 @@ void movilEquilibrado(int pi, int di, int pd, int dd,
     }
     
     peso = pi + pd;
-    equilibrado = equilIz && equilDr && (pi * di == pd * dd)
+    equilibrado = equilIz && equilDr && (pi * di == pd * dd);
     
     
 }
 
+bool resuelve(){
+    int pi, di, pd, dd;
+    cin >> pi >> di >> pd >> dd;
+    if((pi == 0) && (di == 0) && (pd == 0) && (dd == 0))
+        return false;
+    
+    int peso = 0;
+    bool equilibrado = true;
+    movilEquilibrado(pi, di, pd, dd, peso, equilibrado);
+    if(equilibrado)
+        cout << "SI" << endl;
+    else
+        cout << "NO" << endl;
+    
+    return true;
+}
 
+
+
+int main(){
+    while(resuelve())
+        ;
+    return 0;
+}
 
 
 
@@ -176,7 +199,7 @@ void movilEquilibrado(int pi, int di, int pd, int dd,
 //    int pi2, di2, pd2, dd2;
 //    int pesoIz;
 //    bool equilIz;
-//    
+//
 //    if(pi == 0){
 //        cin >> pi2 >> di2 >> pd2 >> dd2;
 //        movilEquilibrado(pi2, di2, pd2, dd2, pesoIz, equilIz);
@@ -204,32 +227,6 @@ void movilEquilibrado(int pi, int di, int pd, int dd,
 
 
 
-
-
-
-
-bool resuelve(){
-    int pi, di, pd, dd;
-    cin >> pi >> di >> pd >> dd;
-    if((pi == 0) && (di == 0) && (pd == 0) && (dd == 0))
-        return false;
-    
-    int peso;
-    bool equilibrado;
-    movilEquilibrado(pi, pd, dd, peso, equilibrado);
-    if(equilibrado)
-        cout << "SI" << endl;
-    else
-        cout << "NO" << endl;
-}
-
-
-
-int main(){
-    while(resuelve())
-        ;
-    return 0;
-}
 
 
 
