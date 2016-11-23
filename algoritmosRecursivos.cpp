ed9a10ec4bca28b138d2a19d56b168ba3997becf
PabloBlanco10 <pablo_bp94@hotmail.com>
2016-11-23 17:03:07 +0100

    somethng

--- algoritmosRecursivos.cpp
+++ algoritmosRecursivos.cpp
@@ -44,15 +44,64 @@ int binarioADecimal(int n){
     return r;
 }
 
+//ejercicio 16 Existe algun v[i] = i
+
+bool dieciseis(int v[], int n, int i){
+    bool r;
+    
+    if(v[i] == i)
+        return true;
+    else if (i < n - 1){
+        i++;
+        r = dieciseis(v, n, i);
+    }
+    else if(i >= n)
+        return false;
+    return r;
+}
+
+
+// ejercicio 1
+// lo ordeno con algoritmo de mezcla, lo recorro comparando la diferencia,
+// y voy actualizando variables
+// coste n log n : log n para ordenar y n para recorrer
+// no se puede hacer en complejidad orden de n
+
+
+// ejercicio 2
+// lo ordeno y cojo primero y ultimo
+// si se puede hacer en orden de n, lo recorro y cojo el minimo y el maximo
+
+
+// ejercicio 7 dosfibonacci
+// dosFib(10, r, s) ----- r = fib(10) --- s = fib(11)
+// caso base = dosFib(0, 0, 1)
+
+
+
 bool resuelve(){
     int n;
     //cout << 13 % 2 << endl << 10 / 10 << endl;
     cin >> n;
+    int v[1000];
+    
     
     while(n>=0){
        // cout <<  binarioADecimal(n) << endl;
-        decimalABinario(n);
-        cout << endl;
+//        decimalABinario(n);
+//        cout << endl;
+//        cin >> n;
+        for(int j = 0; j < n; j++){
+            int aux;
+            cin >> aux;
+            v[j] = aux;
+        }
+        
+        if(dieciseis(v,n,0))
+            cout << "si" << endl;
+        else
+            cout << "no" << endl;
+        
         cin >> n;
     }
     return false;
