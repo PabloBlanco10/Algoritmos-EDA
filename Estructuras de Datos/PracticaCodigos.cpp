6913d44e85c844127a8f9de8d0a05234b19d60ea
Pablo Blanco Peris <pabblanc@ucm.es>
2017-04-27 01:10:33 +0200

    .

---
+++ Estructuras de Datos/PracticaCodigos.cpp
@@ -0,0 +1,28 @@
+//
+//  PracticaCodigos.cpp
+//  
+//
+//  Created by Pablo Blanco Peris on 25/4/17.
+//
+//
+
+#include <stdio.h>
+
+
+
+void invierteSecuenciasNoVocales(Lista<char>::Iterator& itModificacion, Pila){
+    
+}
+
+
+void invierteSecuenciasNoVocales(Lista<char> &mensaje){
+    Pila<char> aInvertir;
+    Lista<charZ::Iterator itModificacion = mensaje.begin();
+    
+    while(it != mensaje.end()){
+        char c = it.elem();
+        if(!esVocal(c)){
+            
+        }
+    }
+}
